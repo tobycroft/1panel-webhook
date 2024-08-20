941002e961d3a9afbfadb35d9e3d173ee34d2d60
tuuz <silverhawk@foxmail.com>
2024-08-20 23:13:07 +0800

    webhook

--- main.go
+++ main.go
@@ -20,11 +20,11 @@ func init() {
 			os.Mkdir("./log", 0755)
 		}
 	}
-	s, err := os.Stat("./webhook/")
+	s, err := os.Stat("./exec/")
 	if err != nil {
-		os.Mkdir("./webhook", 0755)
+		os.Mkdir("./exec", 0755)
 	} else if s.IsDir() {
-		os.Mkdir("./webhook", 0755)
+		os.Mkdir("./exec", 0755)
 	}
 }
 
